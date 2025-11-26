6162ef3ef8ffd1b82c44649e9d76d9be54c57ade
tsuroki <tsuroki0210@gmail.com>
2025-11-26 18:12:46 +0900

    responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
+const topNoticeSwiper = new Swiper ('.notice-swiper', {
+    loop : true,
+    autoplay : {
+        delay : 5000,
+    },
+    spaceBetween : 10,
+    direction : 'vertical',
+    slidesPerView : 1,
+    draggable : false,
+})
+
 const bnrSwiper = new Swiper ('.main-bnr', {
     loop : true,
     autoplay : {
@@ -12,3 +23,89 @@ const bnrSwiper = new Swiper ('.main-bnr', {
         nextEl : '.bnr-next',
     }
 })
+
+const specialSwiper = new Swiper ('.special-swiper', {
+    loop : false,
+    autoplay : false,
+    pagination : {
+        el: '.special-bar',
+        type : 'progressbar',
+    },
+    navigation : {
+        prevEl : '.special-prev',
+        nextEl : '.special-next',
+    }
+})
+
+const themeSwiper = new Swiper ('.theme-swiper', {
+    loop : false,
+    autoplay : false,
+    pagination : {
+        el: '.theme-bar',
+        type : 'progressbar',
+    },
+    navigation : {
+        prevEl : '.theme-prev',
+        nextEl : '.theme-next',
+    }
+})
+
+const bizSwiper = new Swiper ('.bizpack-swiper', {
+    loop : false,
+    autoplay : false,
+    pagination : {
+        el: '.bizpack-bar',
+        type : 'progressbar',
+    },
+    navigation : {
+        prevEl : '.bizpack-prev',
+        nextEl : '.bizpack-next',
+    }
+})
+
+const vacaSwiper = new Swiper ('.vacation-swiper', {
+    loop : false,
+    autoplay : false,
+    pagination : {
+        el: '.vacation-bar',
+        type : 'progressbar',
+    },
+    navigation : {
+        prevEl : '.vacation-prev',
+        nextEl : '.vacation-next',
+    }
+})
+
+const searchIcon = document.querySelector('.search')
+
+searchIcon.addEventListener('click',(e)=>{
+    e.preventDefault()
+})
+
+const myPage = document.querySelector('.mypage')
+
+myPage.addEventListener('click',(e)=>{
+    e.preventDefault()
+})
+
+const coupon = document.querySelector('.coupon')
+
+coupon.addEventListener('click',(e)=>{
+    e.preventDefault()
+})
+
+const gnbList = document.querySelectorAll('.gnb > li')
+
+for(let i of gnbList){
+    i.addEventListener('click', (e) => {
+        e.preventDefault()
+    })
+}
+
+const themeList = document.querySelectorAll('.lnb-list > li')
+
+for(let i of themeList){
+    i.addEventListener('click', (e) => {
+        e.preventDefault()
+    })
+}
--- scripts/swiper-js.js
+++
@@ -1,7 +0,0 @@
-const notice = new Swiper('.notice-swiper', {
-    direction: 'vertical',
-    loop: true,
-    autoplay : {
-        delay : 100000,
-    }
-});
